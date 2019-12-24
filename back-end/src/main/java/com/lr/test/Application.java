2d3bd9b5f200aebe135c98e8d7c9e81e4fe6a2a1
kenny.cai <kennycai2836@163.com>
2019-12-24 16:48:33 +0800

    rename TestApplication

--- back-end/src/main/java/com/lr/test/Application.java
+++ back-end/src/main/java/com/lr/test/Application.java
@@ -2,16 +2,14 @@ package com.lr.test;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.lr.test.dao")
-public class TestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
